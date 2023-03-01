9bea8bc5ebcd27be1699715369023f252b8b4014
chenxi <907725402@qq.com>
2023-03-01 17:51:30 +0800

    1.新增用户动态2.消费用户动态3.查询订阅状态

---
+++ imooc-bilibili-dao/src/main/java/com/imooc/bilibili/domain/UserMoment.java
@@ -0,0 +1,15 @@
+package com.imooc.bilibili.domain;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class UserMoment {
+    private Long id;
+    private Long userId;
+    private String type;
+    private Long contentId;
+    private Date createTime;
+    private Date updateTime;
+}
--- imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/config/RocketMQConfig.java
+++ imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/config/RocketMQConfig.java
@@ -1,6 +1,12 @@
 package com.imooc.bilibili.service.config;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.imooc.bilibili.constant.UserMomentsConstant;
+import com.imooc.bilibili.domain.UserFollowing;
+import com.imooc.bilibili.domain.UserMoment;
+import com.imooc.bilibili.service.UserFollowingService;
+import com.mysql.jdbc.StringUtils;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
@@ -13,6 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -23,6 +30,10 @@ public class RocketMQConfig {
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
+
+    @Autowired
+    private UserFollowingService userFollowingService;
+
 
     @Bean("momentsProducer")
     public DefaultMQProducer momentsProducer() throws Exception {
@@ -40,8 +51,25 @@ public class RocketMQConfig {
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-                for (MessageExt msg : msgs) {
-                    System.out.println(msg);
+                MessageExt msg = msgs.get(0);
+                if (msg == null) {
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                }
+                String bodyStr = new String(msg.getBody());
+                UserMoment userMoment = JSONObject.toJavaObject(JSONObject.parseObject(bodyStr), UserMoment.class);
+                Long userId = userMoment.getUserId();
+                List<UserFollowing> fanList = userFollowingService.getUserFans(userId);
+                for (UserFollowing fan : fanList) {
+                    String key = "subscribed-" + fan.getUserId();
+                    String subscribedListStr = redisTemplate.opsForValue().get(key);
+                    List<UserMoment> subscribedList;
+                    if (StringUtils.isNullOrEmpty(subscribedListStr)) {
+                        subscribedList = new ArrayList<>();
+                    } else {
+                        subscribedList = JSONArray.parseArray(subscribedListStr, UserMoment.class);
+                    }
+                    subscribedList.add(userMoment);
+                    redisTemplate.opsForValue().set(key, JSONObject.toJSONString(subscribedList));
                 }
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
